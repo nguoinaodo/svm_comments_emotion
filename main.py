ba1132144b7b1c000ced81a0736191269f7de5c6
nguoinaodo <luugu196@gmail.com>
2017-10-30 15:43:46 +0700

    Commit

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ from preprocessing.vectorize import train_features, \
 clf = OneVsRestClassifier(DecisionTreeClassifier())
 # clf = GaussianNB()
 # clf = OneVsRestClassifier(SVC(kernel='linear'), param_grid)
-# clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-# 		hidden_layer_sizes=(10, 10), random_state=1) # 200PCA
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+		hidden_layer_sizes=(20, 30), random_state=1) # 200PCA
 # Model
 # param_grid = {'C': [1e1, 1e2, 1e3, 5e3, 1e4],\
 #         'gamma': [0.0001, 0.0005, 0.001, .01, .1]}#, 0.001, 0.005, 0.01, 0.1]}
 # clf = GridSearchCV(SVC(kernel='linear'), param_grid)
 # clf = SGDClassifier()
 # Fit
-for n in [500, 600, 700, 800, 900, 1000, 1500, 2000]:
+for n in [100, 200]:
 	pca = PCA(n_components=n, svd_solver='full').fit(train_features.toarray())
 	with open('pca.pkl', 'wb') as f:
 		pickle.dump(pca, f)
@@ -35,7 +35,6 @@ for n in [500, 600, 700, 800, 900, 1000, 1500, 2000]:
 	clf.fit(train_features_pca, train_labels)
 	# Predict
 	print clf.score(test_features_pca, test_labels)
-	print np.where(clf.predict(test_features_pca) == 1)
 
 
 
